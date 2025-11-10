c56ead9732b2b055622b98fd8d07055beba56598
RaviTiwari05 <ravifzd05@gmail.com>
2025-11-11 00:12:29 +0530

    seeding

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,77 @@
+import prisma from "../src/infrastructure/db/prismaClient";
+
+async function main() {
+  console.log("🌱 Seeding routes...");
+
+  await prisma.route.createMany({
+    data: [
+      {
+        routeId: "R001",
+        vesselType: "Container",
+        fuelType: "HFO",
+        year: 2024,
+        ghgIntensity: 91.0,
+        fuelConsumption: 5000,
+        distance: 12000,
+        totalEmissions: 4500,
+        isBaseline: true
+      },
+      {
+        routeId: "R002",
+        vesselType: "BulkCarrier",
+        fuelType: "LNG",
+        year: 2024,
+        ghgIntensity: 88.0,
+        fuelConsumption: 4800,
+        distance: 11500,
+        totalEmissions: 4200,
+        isBaseline: false
+      },
+      {
+        routeId: "R003",
+        vesselType: "Tanker",
+        fuelType: "MGO",
+        year: 2024,
+        ghgIntensity: 93.5,
+        fuelConsumption: 5100,
+        distance: 12500,
+        totalEmissions: 4700,
+        isBaseline: false
+      },
+      {
+        routeId: "R004",
+        vesselType: "RoRo",
+        fuelType: "HFO",
+        year: 2025,
+        ghgIntensity: 89.2,
+        fuelConsumption: 4900,
+        distance: 11800,
+        totalEmissions: 4300,
+        isBaseline: false
+      },
+      {
+        routeId: "R005",
+        vesselType: "Container",
+        fuelType: "LNG",
+        year: 2025,
+        ghgIntensity: 90.5,
+        fuelConsumption: 4950,
+        distance: 11900,
+        totalEmissions: 4400,
+        isBaseline: false
+      }
+    ]
+  });
+
+  console.log("✅ Routes seeded.");
+}
+
+main()
+  .then(() => {
+    console.log("✅ Seed completed.");
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error("❌ Seed error", e);
+    process.exit(1);
+  });
